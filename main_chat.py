38b0eb3017b48fd9a7a00ba720c67312860d416f
coderfast <coderfast@gmail.com>
2024-11-25 01:34:14 +0100

    load chat model and ignore warnings

--- main_chat.py
+++ main_chat.py
@@ -6,6 +6,7 @@ import keyboard
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import warnings
 import multiprocessing as mp
 from torch.utils.data import Dataset, DataLoader
 from aimlloder import *
@@ -25,6 +26,9 @@ class MainChat:
 
     def __init__(self, args):
         
+        # Don't show the next wanings
+        warnings.filterwarnings("ignore", message=".*clean_up_tokenization_spaces.*", category=FutureWarning)
+        
         # Check if the tokenizer and tokenized data files exist
         self.tokenizer = None
         self.tokenized_data = None
@@ -36,16 +40,6 @@ class MainChat:
         self.num_cores = args.num_cores
         self.num_threads = args.num_threads
         
-        # Prepare data for PyTorch
-        print(f"Preparing data for PyTorch, encoding data")
-        encoded_data = []
-        # for i in range(len(merged_dataset)):
-        #     if 'output' in merged_dataset:
-        #         encoded_data.append((self.tokenizer.encode(str(merged_dataset['input_ids'][i])), self.tokenizer.encode(str(merged_dataset['output'][i]))))
-        #     else:
-        #         encoded_data.append((self.tokenizer.encode(str(merged_dataset['input_ids'][i])), self.tokenizer.encode(str(""))))
-        # print(f"Prepared data for PyTorch, encoded data")
-
         # print(f"create dataloader")
         # Create DataLoader with custom collate function
         # dataloader = DataLoader(encoded_data, batch_size=32, shuffle=True, collate_fn=self.collate_fn)
@@ -70,15 +64,19 @@ class MainChat:
         print(f"Number of CPU cores: {var_num_cores}")
         print(f"Number of CPU threads: {var_num_threads}")
 
-        # Initialize model
+        # Initialize and Load pre-trained model
         # device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
         device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "xla" if "XLA_AVAILABLE" in os.environ else "rocm" if torch.version.hip is not None else "cpu" if torch.backends.mkldnn.is_available() else "opengl" if torch.backends.opengl.is_available() else "opencl" if torch.backends.opencl.is_available() else "ideep" if torch.backends.ideep.is_available() else "hip" if torch.version.hip is not None else "ve" if torch.version.ve is not None else "fpga" if torch.version.fpga is not None else "ort" if torch.version.ort is not None else "lazy" if torch.version.lazy is not None else "vulkan" if torch.version.vulkan is not None else "meta" if torch.version.meta is not None else "hpu" if torch.version.hpu is not None else "mtia" if torch.version.mtia is not None else "privateuse" if torch.version.privateuse is not None else "openmp" if torch.backends.openmp.is_available() else "cpu")
-        model = ChatModel(self.tokenizer, embed_size=128, hidden_size=256).to(device)
+        self.model = ChatModel(self.tokenizer, embed_size=128, hidden_size=256).to(device)        
+        model_path = 'chat_model.pth'
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
+
         print(f"Using {device} device")
 
         # Define loss and optimizer
         criterion = nn.CrossEntropyLoss(ignore_index=self.tokenizer.word2idx['<PAD>'])
-        optimizer = optim.Adam(model.parameters())
+        optimizer = optim.Adam(self.model.parameters())
 
         # Initialize Dialogue Manager
         intent_classifier = pipeline('text-classification', model='nlptown/bert-base-multilingual-uncased-sentiment')
@@ -89,7 +87,7 @@ class MainChat:
             "occupation": "AI assistant",
             "interests": ["IT technology", "MS Office", "Libre Office", "Games", "Humanity simulation"]
         }
-        self.dialogue_manager = DialogueManager(model, device, self.tokenizer, intent_classifier, sentiment_analyzer, persona)
+        self.dialogue_manager = DialogueManager(self.model, device, self.tokenizer, intent_classifier, sentiment_analyzer, persona)
 
 
     # Collate function for DataLoader
@@ -98,15 +96,15 @@ class MainChat:
         input_sequences, output_sequences = zip(*batch)
 
         # Find max lengths
-        # max_input_len = max(len(seq) for seq in input_sequences)
-        # max_output_len = max(len(seq) for seq in output_sequences)
-        max_len = max(max(len(seq) for seq in input_sequences), max(len(seq) for seq in output_sequences))
+        max_input_len = max(len(seq) for seq in input_sequences)
+        max_output_len = max(len(seq) for seq in output_sequences)
+        # max_len = max(max(len(seq) for seq in input_sequences), max(len(seq) for seq in output_sequences))
 
         # Pad sequences
-        # padded_inputs = [seq + [tokenizer.word2idx['<PAD>']] * (max_input_len - len(seq)) for seq in input_sequences]
-        # padded_outputs = [seq + [tokenizer.word2idx['<PAD>']] * (max_output_len - len(seq)) for seq in output_sequences]
-        padded_inputs = [seq + [self.tokenizer.word2idx['<PAD>']] * (max_len - len(seq)) for seq in input_sequences]
-        padded_outputs = [seq + [self.tokenizer.word2idx['<PAD>']] * (max_len - len(seq)) for seq in output_sequences]
+        padded_inputs = [seq + [self.tokenizer.word2idx['<PAD>']] * (max_input_len - len(seq)) for seq in input_sequences]
+        padded_outputs = [seq + [self.tokenizer.word2idx['<PAD>']] * (max_output_len - len(seq)) for seq in output_sequences]
+        # padded_inputs = [seq + [self.tokenizer.word2idx['<PAD>']] * (max_len - len(seq)) for seq in input_sequences]
+        # padded_outputs = [seq + [self.tokenizer.word2idx['<PAD>']] * (max_len - len(seq)) for seq in output_sequences]
 
         # Convert to tensors
         input_tensor = torch.LongTensor(padded_inputs)
