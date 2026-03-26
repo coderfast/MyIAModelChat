29fc96fbe4b0f1404415f4416a081aaf9b878df0
coderfast <coderfast@gmail.com>
2026-03-26 02:01:46 +0100

    added logging to simple tokenizer

--- simpletokenizer.py
+++ simpletokenizer.py
@@ -5,6 +5,12 @@ import re
 import unicodedata
 from collections import Counter
 from typing import List, Dict, Tuple, Optional
+import logging
+
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class TrieNode:
     def __init__(self):
