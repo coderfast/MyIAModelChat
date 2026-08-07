4545ee5980ec99eae899c3055d674faa4ff3ad70
coderfast <coderfast@gmail.com>
2026-08-07 19:00:45 +0200

    Optimize .gitignore + add chat_model_hf config/scripts
    - .gitignore: ignore models/**/*.bin, *.onnx, *.safetensors, intent/sentiment/, DOCS/fonts/
    - Track chat_model_hf config.json, conversion scripts, tokenizer vocab

---
+++ models/exported/chat_model_hf/convert_to_gguf.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Convert chat_model to GGUF
+python "G:\PROJECTS\MyIAModelChat\models\exported\convert.py" "G:\PROJECTS\MyIAModelChat\models\exported\chat_model_hf" --outfile "G:\PROJECTS\MyIAModelChat\models\exported\chat_model_q8_k.gguf" --outtype q8_k
